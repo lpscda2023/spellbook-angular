b08ec7fceda1623ffca2fec53843a39711f7aa1b
IdrissaSoumare <soumare.id@laposte.net>
2024-05-03 16:19:43 +0200

    mise en place du input

--- src/data.ts
+++ src/data.ts
@@ -14,6 +14,12 @@ export const posts: ListElement[] = [
         link: 'https://stackblitz-starters-k8lepf.stackblitz.io',
         difficulty: 0,
       },
+
+      {
+        title: "make an input into the application's head",
+        link: 'https://stackblitz.com/edit/stackblitz-starters-wnhj6u?file=src%2Fapp%2Fapp.component.html',
+        difficulty: 1,
+      }
     ],
   },
   {
