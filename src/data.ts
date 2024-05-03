bbef5499bc72e230ede85d19b3a3433e45a3a616
manueitsmy <manueitsmy@gmail.com>
2024-05-03 17:38:05 +0200

    Correction place

--- src/data.ts
+++ src/data.ts
@@ -39,7 +39,12 @@ export const posts: ListElement[] = [
         title: "make an input into the application's head",
         link: 'https://stackblitz.com/edit/stackblitz-starters-wnhj6u?file=src%2Fapp%2Fapp.component.html',
         difficulty: 1,  
-      }
+      },
+      {
+        title: 'I want to react to a click even',
+        link: 'https://stackblitz.com/edit/stackblitz-starters-usgxxa',
+        difficulty: 0,
+      },
     ],
   },
   {
@@ -48,11 +53,6 @@ export const posts: ListElement[] = [
       {
         title: 'Template pour créer une nouvelle ressource Stackblitz',
         link: 'https://stackblitz.com/edit/stackblitz-starters-c9hsvm',
-        difficulty: 0,
-      },
-      {
-        title: 'I want to react to a click even',
-        link: 'https://stackblitz.com/edit/stackblitz-starters-usgxxa',
         difficulty: 0,
       },
     ],
